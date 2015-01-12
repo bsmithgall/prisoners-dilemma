037128c44cb3a746b8721a3f617ab76b5394620a
bsmithgall <ben.smithgall@gmail.com>
2015-01-12 12:14:29 -0500

    added bare downing strategy

---
+++ strategies/downing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def calculate_probability(history):
+    '''
+    Calcluate the conditional probability of opposition's defection
+    from the history
+    '''
+    pass
+
+def main(strategy_a, strategy_b, history):
+    '''
+    From Axelrod: "The idea is that if the other player
+    does not seem responsive to what DOWNING is doing, DOWNING
+    will try to get away with whatever it can by defecting. On
+    the other hand, if the other player does seem responsive,
+    DOWNING will cooperate."
+    '''
+    pass
